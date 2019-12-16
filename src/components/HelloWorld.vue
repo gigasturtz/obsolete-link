ae2a52587b3c75a0471b4780a1a9473e70969efb
Nicholas Sturtzel <gigasturtz@Nicholass-MacBook-Pro-7.local>
2019-12-16 13:19:07 -0500

    adds image to events

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,13 @@
 <template>
-<div>
-  <p>{{ events }}</p>
+<div class="events">
+  <ul v-for='event in events' v-bind:key="event.id">
+    <img class="show" :src="event.image" />
+    <li>{{ event.description }}</li>
+    <li>{{ event.date }}</li>
+    <li>{{ event.time }}</li>
+    <li>{{ event.location }}</li>
+    <li>{{ event.cover }}</li>
+  </ul>
 </div>
 </template>
 
@@ -26,18 +33,33 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
+  margin: 20px 0 0;
   list-style-type: none;
   padding: 0;
 }
 li {
-  display: inline-block;
+  /* display: inline-block; */
   margin: 0 10px;
 }
 a {
   color: #42b983;
+}
+
+img {
+  display: block;
+}
+
+.events {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 25vw);
+  grid-gap: 15px;
+}
+
+.show {
+  height: 100%;
+  width: 100%;
+  object-fit: contain;  
 }
 </style>
