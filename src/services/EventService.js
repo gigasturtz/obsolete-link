a5d34d051eacd34e017727e46a16cc7dc3133c45
Nick Sturtzel <gigasturtz@gmail.com>
2020-01-28 13:26:27 -0500

    creates environment variables for gateway url

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -2,26 +2,26 @@ const fetch = require('node-fetch')
 
 export function getEvents() {
     return new Promise((resolve, reject) => {
-        fetch('http://localhost:3000/event')
-        .then(response => {
-            resolve(response.json())
-        })
-        .catch(error => {reject(error)})
+        fetch(`${process.env.VUE_APP_GATEWAY_BASE_URL}/event`)
+            .then(response => {
+                resolve(response.json())
+            })
+            .catch(error => { reject(error) })
     })
 }
 
 export function addEvent(event) {
     return new Promise((resolve, reject) => {
         const data = JSON.stringify(event)
-        fetch('http://localhost:3000/event', 
-        {
-        method: 'post',
-        body: data,
-        headers: { 'Content-Type': 'application/json' }
-        })
-        .then(response => {
-            resolve(response.json())
-        })
-        .catch(error => {reject(error)})
+        fetch(`${process.env.VUE_APP_GATEWAY_BASE_URL}/event`,
+            {
+                method: 'post',
+                body: data,
+                headers: { 'Content-Type': 'application/json' }
+            })
+            .then(response => {
+                resolve(response.json())
+            })
+            .catch(error => { reject(error) })
     })
 }   
